18cfd1de490c4ac996adc5ee01b8d1a6226970cd
starJeet000 <soumyajitmukherjee000@gmail.com>
2024-09-21 00:20:05 +0530

    1-backend completed

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+#TODO: Update this file for deployment
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#api routes
+import routes
+
+with app.app_context():
+    db.create_all()
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
